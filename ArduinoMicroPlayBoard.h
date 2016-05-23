e53542a56441d13940089c46be6d121d13957060
Fran Acien <amil101ftw@gmail.com>
2016-05-23 10:42:29 +0200

    Initial commit

---
+++ ArduinoMicroPlayBoard.h
@@ -0,0 +1,15 @@
+/*
+  Arduino Micro Play Board Library for programming ArduinoMicro Play Board more easily.
+  Created by Fran Acién 23 may 2016
+  Released into th epublic domain
+*/
+#ifndef ArduinoMicroPlayBoard_h
+#define ArduinoMicroPlayBoard_h
+
+#include "Arduino.h"
+
+class AruinoMicroPlayBoard{
+
+};
+
+#endif
